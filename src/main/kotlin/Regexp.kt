f5d25ad0ed373ba3b35a13e4c3903477290da05c
Nikolay <56501319+NikolayOvcharenko@users.noreply.github.com>
2020-07-29 17:30:17 +0300

    fun regexp Date

--- src/main/kotlin/Regexp.kt
+++ src/main/kotlin/Regexp.kt
@@ -1,7 +1,21 @@
 fun main() {
-    val line = "abc 123 def"
 
-    val regexp = """.\d{3}."""
+    println(date("12 MAR 2020"))
+    // val line = "abc 123 def"
+    println(date("12 03 2020"))
+    // val regexp = """.*\d{3}.*"""
 
-    
+    // val pattern = regexp.toRegex()
+
+    // println(line.matches(pattern))
+
+}
+
+fun date (date: String) :Boolean {
+
+    val month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
+
+    val regexp = """\d{2} $month \d{4}"""
+
+    return date.matches(regexp.toRegex())
 }
